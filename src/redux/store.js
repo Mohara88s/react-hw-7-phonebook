cb588ae992a89e9b0ce713d05941f3afa05880cf
Mohara88s <mohara88s@gmail.com>
2021-08-24 13:31:34 +0300

    local storage delete

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contacts/contacts-reducer';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
 
 const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
-
-export default { store, persistor };
+export default store;
