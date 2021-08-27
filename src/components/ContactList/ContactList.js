426044b4d7bf5ea8db3ccf413cc9f5ade0dff86d
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-08-27 15:35:46 +0300

    1

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,34 +1,28 @@
-import React from 'react';
 import Contact from '../Contact/Contact';
 import styles from './ContactList.module.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from '../../redux/contacts/contacts-operaions';
 import { useEffect } from 'react';
+import contactsSelectors from '../../redux/contacts/contacts-selectors';
 
 const ContactList = () => {
-  const error = useSelector(state => state.contacts.error);
-  const loading = useSelector(state => state.contacts.loading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContacts());
   }, []);
-
-  const contacts = useSelector(state => state.contacts.items);
-  const filter = useSelector(state => state.contacts.filter);
+  const error = contactsSelectors.getError();
+  const loading = contactsSelectors.getLoading();
+  const filtredContacts = contactsSelectors.getFiltredContacts();
   return (
     <>
       {loading && <h2 className={styles.loading}>thinking...</h2>}
       {error && <h2 className={styles.error}>{error}</h2>}
       <ul className={styles.ContactList}>
-        {contacts
-          .filter(contact =>
-            contact.name.toLowerCase().includes(filter.toLowerCase()),
-          )
-          .map(({ id, name, number }) => (
-            <li key={id} className={styles.item}>
-              <Contact id={id} name={name} number={number} />
-            </li>
-          ))}
+        {filtredContacts.map(({ id, name, number }) => (
+          <li key={id} className={styles.item}>
+            <Contact id={id} name={name} number={number} />
+          </li>
+        ))}
       </ul>
     </>
   );
